971b9f32aac56a4fdc3aa23e7dc686a32e82067f
Antoine <20175226+antooinex@users.noreply.github.com>
2023-10-02 15:13:19 -0400

    avancée dans la créatio de la partie et du joueur 1

---
+++ LOG320_TicTacToe/src/LOG320_TicTacToe/Player.java
@@ -0,0 +1,41 @@
+package LOG320_TicTacToe;
+
+import java.util.Scanner;
+
+public class Player {
+	
+	private String name = "null";
+	private Mark mark;
+
+	Player(){
+		System.out.println("Veuillez entrer votre nom :");
+		Scanner scanner = new Scanner(System.in);
+		this.name = scanner.nextLine();
+		this.mark = Mark.EMPTY;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setMark(String choice) {
+		if(choice.equals("X")) {
+			this.mark = Mark.X;
+		}
+		else {
+			this.mark = Mark.O;
+		}
+	}
+	
+	public String printMark() {
+		if(this.mark == Mark.X) {
+			return "X";
+		}
+		else if(this.mark == Mark.O){
+			return "O";
+		}
+		else {
+			return "null";
+		}
+	}
+}
